d5e5f927071b5a2b6cf2f51ac19b4f0caf02fb03
Jaesuk <jaesuk95@outlook.com>
2023-08-27 14:31:55 +0900

    Initial Commit

---
+++ FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by Jaesuk Lee on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
