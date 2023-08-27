c6c1cabeb725c945ebec1753ace95b97dc173b1e
Jaesuk <jaesuk95@outlook.com>
2023-08-27 15:58:56 +0900

    iOS timer setting, function, systemSound

--- FirstNewApp/ViewController.swift
+++ FirstNewApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,12 +16,16 @@ class ViewController: UIViewController {
     // slider (type : UISlider)
     @IBOutlet weak var slider: UISlider!
     
+    // google search
+    weak var timer: Timer?
+    
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         configureUI()
-        
     }
     
     // slide
@@ -30,13 +35,56 @@ class ViewController: UIViewController {
         let seconds = Int(slider.value * 60) // 0.0 ~ 1.0
 //        print(seconds)
         mainLabel.text = "\(seconds) 초"
+        number = seconds
     }
+    
+
     
     // start button
     @IBAction func startButtonTapped(_ sender: UIButton) {
         // 1초씩 지나갈때마다 무언가를 실행
+        timer?.invalidate() // just in case you had existing 'Timer' , 'invalidte' it before we lose our reference to it
+        
+        // withTimeInterval: 1.0 <- interval time (예 1초 뒤에 repeat 실행)
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
+            // 반복 하고 싶은 코드
+            
+            print(number)
+            
+            if number > 0 {
+                number -= 1
+                
+                // slider 줄여야 되고
+                slider.value = Float(number) / Float(60)
+                
+                // 레이블도 줄여야 한다
+                mainLabel.text = "\(number) 초"
+                
+            } else {
+                
+                // 0초가 되면 소리까지
+//                number = 0
+//                mainLabel.text = "선택하세요"
+                
+                // timer 끝
+                stopTimer()
+                
+                // 소리를 나게 해야된다
+                AudioServicesPlayAlertSound(SystemSoundID(1322))
+                
+                configureUI()
+            }
+            
+        }
+        
         
     }
+
+    func stopTimer () {
+        timer?.invalidate()
+    }
+
+    
     
     // reset button
     @IBAction func resetButtonTapped(_ sender: UIButton) {
@@ -64,6 +112,10 @@ class ViewController: UIViewController {
         
         // slider 의 중간 설정
         slider.setValue(0.5, animated: true)
+        
+        number = 0
+        
+        timer?.invalidate()
     }
 }
 
