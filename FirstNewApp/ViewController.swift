7fe61e329f1cfac55524eab8acbc80feb902f0f1
Jaesuk <jaesuk95@outlook.com>
2023-08-27 14:59:15 +0900

    function class 사용법

--- FirstNewApp/ViewController.swift
+++ FirstNewApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        configureUI()
+        
     }
 
 
+    func configureUI() {
+        mainLabel.text = "선택하세요"
+    }
+    
+    // slide
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    // start button
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    // reset button
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
